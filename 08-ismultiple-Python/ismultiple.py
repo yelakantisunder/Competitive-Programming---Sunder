3197c3f7d9c7ef3aa6c000d2fe249ad4eafa43b2
yelakantisunder <Ysunder7@gmail.com>
2021-07-12 19:59:12 +0530

    done

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -3,7 +3,13 @@
 # Note that 0 is a multiple of every integer including itself. 
 # Also, you should make constructive use of the isFactor function you just wrote above.
 
-
-
 def fun_ismultiple(m, n):
-	return False # replace with your solution
+	if(m==0):
+		return True
+	elif(n==0):
+		return False
+	elif(m%n==0):
+		return True
+	else:
+		return False
+		 # replace with your solution
