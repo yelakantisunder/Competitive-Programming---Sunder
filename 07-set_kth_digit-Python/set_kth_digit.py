15a60e593251320b3c282d4e6adf4629cb23d470
yelakantisunder <Ysunder7@gmail.com>
2021-07-14 08:43:26 +0530

    done

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -7,5 +7,23 @@
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+	flag = False
+	if(n < 0):
+		n = -n
+		flag = True
+	count = 0
+	res = 0
+	while(count<=k or n > 0):
+		r = n % 10
+		if count == k:
+			res = res + (d * (10 ** count))
+		else:
+			res = res + (r * (10 ** count))
+		count += 1
+		n = n // 10
+
+	if flag:
+		res = -res
+
+	return res
 
