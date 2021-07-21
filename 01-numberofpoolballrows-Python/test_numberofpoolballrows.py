4c2346adc141d514e8b4aa7cfa54687eff4a5b18
yelakantisunder <Ysunder7@gmail.com>
2021-07-21 19:11:01 +0530

    done

--- 01-numberofpoolballrows-Python/test_numberofpoolballrows.py
+++ 01-numberofpoolballrows-Python/test_numberofpoolballrows.py
@@ -1,31 +1,28 @@
 import os,sys
 sys.path.append(os.getcwd())
-from numberofpoolballrows import fun_numberofpoolballrows
+from numberofpoolballs import numberofpoolballs
 import pytest
 
 
 @pytest.mark.parametrize('n, result',[
 	(1, 1),
-	(3, 2),
-	(6, 3),
-	(7, 4),
-	(10, 4),
-	(15, 5),
-	(21, 6),
-	(28, 7),
-	(36, 8),
-	(45, 9),
-	(55, 10),
-	(56, 11),
-	(66, 11),
-	(78, 12),
-	(77, 12),
-	(91, 13),
-	(105, 14),
-	(120, 15),
-	(5050, 100),
-	(500500, 1000),
-	(4999950000, 99999)
+	(2, 3),
+	(3, 6),
+	(4, 10),
+	(5, 15),
+	(6, 21),
+	(7, 28),
+	(8, 36),
+	(9, 45),
+	(10, 55),
+	(11, 66),
+	(12, 78),
+	(13, 91),
+	(14, 105),
+	(15, 120),
+	(100, 5050),
+	(1000, 500500),
+	(99999, 4999950000)
 ])
-def test_fun_numberofpoolballrows(n, result):
-    assert fun_numberofpoolballrows(n) == result
+def test_numberofpoolballs(n, result):
+    assert numberofpoolballs(n) == result
