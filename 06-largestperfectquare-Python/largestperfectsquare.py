64b052bcfbb692a10f91ff3ed6862c1aac33e0cc
yelakantisunder <Ysunder7@gmail.com>
2021-07-21 17:34:43 +0530

    done

--- 06-largestperfectquare-Python/largestperfectsquare.py
+++ 06-largestperfectquare-Python/largestperfectsquare.py
@@ -7,6 +7,18 @@
 # Hint: you may wish to use a similar approach to how you solved isPerfectSquare on the hw.
 # Another hint: This can be written using just one or two lines of Python.
 
+import math
 def largestperfectsquare(n):
-	# your code goes here
-	pass
+	return int(math.sqrt(n))*int(math.sqrt(n))
+
+    	
+    # while n > 0:
+    #     i = 1
+    #     while i <= n//2:
+    #         square = i*i
+    #         if square == n:
+    #             return square
+    #         else:
+    #             i += 1
+    #     n -= 1
+    # return 1
