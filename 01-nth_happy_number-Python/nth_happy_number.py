883832d776e8bca416aa6be650a5e471d483ed7d
yelakantisunder <Ysunder7@gmail.com>
2021-07-23 21:44:31 +0530

    done

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -15,46 +15,72 @@
 # assert(nth_happy_number(8) == 31)
 
 
-def numSquareSum(n):
-    squareSum = 0;
-    while(n>0):
-        squareSum += (n % 10) * (n % 10);
-        n = int(n / 10);
-    return squareSum
+# def numSquareSum(n):
+#     squareSum = 0;
+#     while(n>0):
+#         squareSum += (n % 10) * (n % 10);
+#         n = int(n / 10);
+#     return squareSum
 
-# method return true if
-# n is Happy number
+# # method return true if
+# # n is Happy number
+# def ishappynumber(n):
+#     # initialize slow
+#     # and fast by n
+#     slow = n;
+#     fast = n;
+#     while(True):
+
+#         # move slow number
+#         # by one iteration
+#         slow = numSquareSum(slow);
+#         # move fast number
+#         # by two iteration
+#         fast = numSquareSum(numSquareSum(fast));
+#         if(slow != fast):
+#             continue;
+#         else:
+#             break;
+
+#     # if both number meet at 1,
+#     # then return true
+#     if (slow == 1):
+#         return True
+#     else:
+#         return False
+
+# def nth_happy_number(n):
+    
+#     found = 1
+#     guess = 0
+#     while (found <= abs(n)):
+#         guess += 1
+#         if(ishappynumber(guess)):
+#             found += 1
+#     return guess
+
+
 def ishappynumber(n):
-    # initialize slow
-    # and fast by n
-    slow = n;
-    fast = n;
-    while(True):
-
-        # move slow number
-        # by one iteration
-        slow = numSquareSum(slow);
-        # move fast number
-        # by two iteration
-        fast = numSquareSum(numSquareSum(fast));
-        if(slow != fast):
-            continue;
-        else:
-            break;
-
-    # if both number meet at 1,
-    # then return true
-    if (slow == 1):
-        return True
-    else:
+    # your code goes here
+    if n<1:
         return False
-
+    sum = 0
+    while(n>0):
+        sum+= (n%10)**2
+        n = n//10
+    if sum == 1:
+        return True
+    elif sum < 10:
+        return False
+    else:
+        return ishappynumber(sum)
 def nth_happy_number(n):
     
-    found = 1
+    found = 0
     guess = 0
-    while (found <= abs(n)):
+    while (found < abs(n)):
         guess += 1
         if(ishappynumber(guess)):
             found += 1
     return guess
+print(nth_happy_number(2))
