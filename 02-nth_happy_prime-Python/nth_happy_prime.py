85aaa34e2126f995fe0a0088ee0cf521001bc43a
yelakantisunder <Ysunder7@gmail.com>
2021-07-23 21:38:34 +0530

    done

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -3,6 +3,65 @@
 # and returns the nth happy prime number (where the 0th happy prime number is 7).
 
 
+# def isprime(x):
+#     count=0
+#     if x>1:
+#         for i in range(1,x+1):
+#             if(x%i)==0:
+#                 count = count+1
+#         if count == 2:
+#             return True
+#         else:
+#             return False
+  
+# #helper function to find the squares of digits
+# def numSquareSum(n):
+#     squareSum = 0;
+#     while(n>0):
+#         squareSum += (n % 10) * (n % 10);
+#         n = int(n / 10);
+#     return squareSum
+
+# # method return true if
+# # n is Happy number
+# def ishappynumber(n):
+#     # initialize slow
+#     # and fast by n
+#     slow = n;
+#     fast = n;
+#     while(True):
+
+#         # move slow number
+#         # by one iteration
+#         slow = numSquareSum(slow);
+#         # move fast number
+#         # by two iteration
+#         fast = numSquareSum(numSquareSum(fast));
+#         if(slow != fast):
+#             continue;
+#         else:
+#             break;
+
+#     # if both number meet at 1,
+#     # then return true
+#     if (slow == 1):
+#         return True
+#     else:
+#         return False
+
+# #finding nth happy primes
+
+# def fun_nth_happy_prime(n):
+#     found = 0
+#     guess = 0
+#     while (found <= n):
+#         guess += 1
+#         #print(guess)
+#         if ((ishappynumber(guess)) and (isprime(guess))):
+#             found += 1
+#             #print(guess)
+#     return guess
+
 def isprime(x):
     count=0
     if x>1:
@@ -13,52 +72,26 @@ def isprime(x):
             return True
         else:
             return False
-  
-#helper function to find the squares of digits
-def numSquareSum(n):
-    squareSum = 0;
-    while(n>0):
-        squareSum += (n % 10) * (n % 10);
-        n = int(n / 10);
-    return squareSum
-
-# method return true if
-# n is Happy number
 def ishappynumber(n):
-    # initialize slow
-    # and fast by n
-    slow = n;
-    fast = n;
-    while(True):
-
-        # move slow number
-        # by one iteration
-        slow = numSquareSum(slow);
-        # move fast number
-        # by two iteration
-        fast = numSquareSum(numSquareSum(fast));
-        if(slow != fast):
-            continue;
-        else:
-            break;
-
-    # if both number meet at 1,
-    # then return true
-    if (slow == 1):
-        return True
-    else:
+    # your code goes here
+    if n<1:
         return False
-
-#finding nth happy primes
-
+    su = 0
+    while(n>0):
+        su+= (n%10)**2
+        n = n//10
+    if su == 1:
+        return True 
+    elif su < 10:
+        return False
+    else:
+        return ishappynumber(su)
 def fun_nth_happy_prime(n):
+    
     found = 0
     guess = 0
     while (found <= n):
         guess += 1
-        #print(guess)
-        if ((ishappynumber(guess)) and (isprime(guess))):
+        if(ishappynumber(guess) and isprime(guess)):
             found += 1
-            #print(guess)
     return guess
-
