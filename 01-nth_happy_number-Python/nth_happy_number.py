6f697c835a7b9433ba4e8f884a2f10a5de66588d
yelakantisunder <Ysunder7@gmail.com>
2021-07-23 21:23:24 +0530

    done

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -15,46 +15,46 @@
 # assert(nth_happy_number(8) == 31)
 
 
-# def numSquareSum(n):
-#     squareSum = 0;
-#     while(n>0):
-#         squareSum += (n % 10) * (n % 10);
-#         n = int(n / 10);
-#     return squareSum
+def numSquareSum(n):
+    squareSum = 0;
+    while(n>0):
+        squareSum += (n % 10) * (n % 10);
+        n = int(n / 10);
+    return squareSum
 
-# # method return true if
-# # n is Happy number
-# def ishappynumber(n):
-#     # initialize slow
-#     # and fast by n
-#     slow = n;
-#     fast = n;
-#     while(True):
+# method return true if
+# n is Happy number
+def ishappynumber(n):
+    # initialize slow
+    # and fast by n
+    slow = n;
+    fast = n;
+    while(True):
 
-#         # move slow number
-#         # by one iteration
-#         slow = numSquareSum(slow);
-#         # move fast number
-#         # by two iteration
-#         fast = numSquareSum(numSquareSum(fast));
-#         if(slow != fast):
-#             continue;
-#         else:
-#             break;
+        # move slow number
+        # by one iteration
+        slow = numSquareSum(slow);
+        # move fast number
+        # by two iteration
+        fast = numSquareSum(numSquareSum(fast));
+        if(slow != fast):
+            continue;
+        else:
+            break;
 
-#     # if both number meet at 1,
-#     # then return true
-#     if (slow == 1):
-#         return True
-#     else:
-#         return False
+    # if both number meet at 1,
+    # then return true
+    if (slow == 1):
+        return True
+    else:
+        return False
 
-# def nth_happy_number(n):
+def nth_happy_number(n):
     
-#     found = 1
-#     guess = 0
-#     while (found <= abs(n)):
-#         guess += 1
-#         if(ishappynumber(guess)):
-#             found += 1
-#     return guess
+    found = 1
+    guess = 0
+    while (found <= abs(n)):
+        guess += 1
+        if(ishappynumber(guess)):
+            found += 1
+    return guess
