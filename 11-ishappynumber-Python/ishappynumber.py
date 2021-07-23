eaf136eccfce0c954f4793265e90d8b6a978f989
yelakantisunder <Ysunder7@gmail.com>
2021-07-23 19:00:59 +0530

    done

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -19,4 +19,15 @@
 
 def ishappynumber(n):
 	# your code goes here
-	pass
+	if n<1:
+		return False
+	sum = 0
+	while(n>0):
+		sum+= (n%10)**2
+		n = n//10
+	if sum == 1:
+		return True 
+	elif sum < 10:
+		return False
+	else:
+		return ishappynumber(sum)
