2fe1b88aa9bd86fc34e6a63e148439e999b3dd75
yelakantisunder <Ysunder7@gmail.com>
2021-07-24 08:49:41 +0530

    done

--- 08-isrotated-Python/isrotated.py
+++ 08-isrotated-Python/isrotated.py
@@ -3,7 +3,11 @@
 # if given String is "XYZ" and "ZXY" then your function should return true, but if the input is 
 # "XYZ" and "YXZ" then return false.
 
-
-def isrotated(str1, str2):
-	#Your code goes here
-	pass
+def isrotated(str1, str2):  
+    if len(str1) != len(str2):
+        return False 
+    str3 = str1 + str1 
+    if (str3.count(str2)> 0):
+        return True
+    else:
+        return False
