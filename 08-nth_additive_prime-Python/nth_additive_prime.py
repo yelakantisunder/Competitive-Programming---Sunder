e848bab0086fd34531cf539be8ec1e23e499e224
yelakantisunder <Ysunder7@gmail.com>
2021-07-27 09:52:23 +0530

    done

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -6,5 +6,31 @@
 
 
 
+
+def getSum(x):
+	sum=0
+	while(x!=0):
+		sum=sum+x%10
+		x=x//10
+	return sum
+
+def isPrime(x):
+	if x<2 :
+		return False
+	for factor in range(2,x):
+		if x%factor == 0:
+			return False
+	return True
+
+	
+
 def fun_nth_additive_prime(n):
-	return 1
+	num =2
+	count = -1
+	while(count<n):
+		sum = getSum(num)
+		if (isPrime(num) and isPrime(sum)):
+			count+=1
+		num+=1
+	
+	return num-1
