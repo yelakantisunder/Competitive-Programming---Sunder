9d1ce18cd1f4d025d1b91d7ccc2df412c0d0cfa1
yelakantisunder <Ysunder7@gmail.com>
2021-07-27 11:02:58 +0530

    done

--- 05-rectangle_overlap-Python/rectangle_overlap.py
+++ 05-rectangle_overlap-Python/rectangle_overlap.py
@@ -7,14 +7,21 @@
 # the x-coordinate goes up while you head right, the y-coordinate goes up while you 
 # head down (so we say that "up is down")
 
+# def fun_rectangle_overlap(left1, top1, width1, height1, left2, top2, width2, height2):
+#     factor1=((left2>left1)and(left2>left1+width1))
+#     factor2=((left1>left2)and(left1>left2+width2))
+#     factor3=((top2>top1)and(top2>top1+height1))
+#     factor4=((top1>top2)and(top1>top2+height2))
+#     if(factor1 or factor2 or factor3 or factor4):
+#         return False
+#     else:
+#         return True
+
 def fun_rectangle_overlap(left1, top1, width1, height1, left2, top2, width2, height2):
-    factor1=((left2>left1)and(left2>left1+width1))
-    factor2=((left1>left2)and(left1>left2+width2))
-    factor3=((top2>top1)and(top2>top1+height1))
-    factor4=((top1>top2)and(top1>top2+height2))
-    if(factor1 or factor2 or factor3 or factor4):
+    if((left1 > height2 and height1 < left2) or (top1 < width2 and width1 > top2)  ):
         return False
-    else:
+    elif((left1 < height2 and height1 > left2) or (top1 > width2 and width1 < top2) ):
         return True
+    return False
 
         
