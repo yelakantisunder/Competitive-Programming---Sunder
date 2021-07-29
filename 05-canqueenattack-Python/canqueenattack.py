009b49e3d921679cf17ec64555a03a34d95dee11
yelakantisunder <Ysunder7@gmail.com>
2021-07-29 16:55:26 +0530

    done

--- 05-canqueenattack-Python/canqueenattack.py
+++ 05-canqueenattack-Python/canqueenattack.py
@@ -4,5 +4,11 @@
 # diagonally.
 
 def canqueenattack(qR, qC, oR, oC):
-	# Your code goes here
-	pass
+    if(qR==oR):
+        return True
+    if(oC==qC):
+        return True
+    if abs(qR-oR)==abs(qC-oC):
+        return True
+    
+    return False
