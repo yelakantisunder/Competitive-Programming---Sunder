b89a1734b3dd6328cbc3116b8f0786420c44387c
yelakantisunder <Ysunder7@gmail.com>
2021-08-05 16:11:42 +0530

    done

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -3,20 +3,38 @@ class Node(object):
         self.value = value
         self.left = None
         self.right = None
-
+ 
 class BST(object):
     def __init__(self, root):
         self.root = Node(root)
-
+ 
     def insert(self, new_val):
-        # Your code goes here
-        pass
-
+        newNode = Node(new_val)
+        if(self.root == None):
+            self.root = newNode
+        else:
+            current = self.root
+            parent = self.root
+            while(current != None):
+                parent = current
+                if(new_val < current.value):
+                    current = current.left
+                else:
+                    current = current.right
+            if(new_val < parent.value):
+                parent.left = newNode
+            else:
+                parent.right = newNode
+ 
     def printSelf(self):
-        # Your code goes here
-        pass
+        print(self.root)
         
     def search(self, find_val):
-        # Your code goes here
-        pass
-
+        while self.root != None:
+            if self.root.value == find_val:
+                return True
+            if self.root.value < find_val:
+                self.root = self.root.right
+            else:
+                self.root = self.root.left
+        return False
