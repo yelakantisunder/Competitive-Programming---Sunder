ff03688253deb0ce12fb77c56f8f0d39d0ffb798
yelakantisunder <Ysunder7@gmail.com>
2021-08-05 16:41:12 +0530

    done

--- 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
+++ 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
@@ -4,5 +4,13 @@
 # of 3 exist, you should return the empty list. You may not use loops/iteration in this problem. 
 
 def recursion_powersof3ton(n):
-	# Your code goes here
-	pass
+    def powerof3ton(n,i=0,l1=[]):
+        if n<=0:
+            return None
+        elif 3**i>n:
+            return l1
+        else:
+            if 3**i<=n:
+                l1.append(3**i)
+            return powerof3ton(n,i+1,l1)    
+    return powerof3ton(int(n))
