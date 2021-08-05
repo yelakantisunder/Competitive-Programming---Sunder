bfea881840cc083dd8182f9d17000e6a2bc70d89
yelakantisunder <Ysunder7@gmail.com>
2021-08-06 00:03:42 +0530

    done

--- friendsOfFriends.py
+++ friendsOfFriends.py
@@ -26,4 +26,21 @@ Thus, in this example, friendsOfFriends should return:
 
 def friendsOfFriends(d):
     # Your code goes here...
-    return None
+    result = dict()
+    for person in d:
+        result[person] = set()
+        for friend in d[person]:
+            for friendsOfFriends in d[friend]:
+                if(friendsOfFriends != person and friendsOfFriends
+                not in d[person]):
+                    result[person].add(friendsOfFriends)
+    return result
+print(friendsOfFriends({
+ 'tyrion': {'arya', 'brienne'}, 
+ 'pod': {'jon'}, 
+ 'brienne': {'tyrion'}, 
+ 'arya': {'tyrion'}, 
+ 'jon': {'pod', 'jaime'}, 
+ 'jaime': {'pod', 'jon'}, 
+ 'ramsay': set()
+}))
