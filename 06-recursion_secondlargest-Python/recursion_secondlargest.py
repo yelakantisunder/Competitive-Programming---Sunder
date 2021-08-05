2dddf00e08a098a7e6def430caa67dad9fb5f819
yelakantisunder <Ysunder7@gmail.com>
2021-08-05 11:50:14 +0530

    done

--- 06-recursion_secondlargest-Python/recursion_secondlargest.py
+++ 06-recursion_secondlargest-Python/recursion_secondlargest.py
@@ -14,5 +14,17 @@
 # values as we recursively traversed the list. Also, you may not use loops/iteration in this problem
 
 def recursion_secondlargest(L):
-	# Your code goes here
-	pass
+    if len(L) <= 1:
+        return None
+    else:
+        return secondlargest(2,L)
+ 
+def secondlargest(n,L=[]):
+    m=max(L)
+    if L.count(m) >= 2:
+        return m
+    if n == 1:
+        return m
+    else:
+        return secondlargest(n-1, [x for x in L if x!=m])
+
